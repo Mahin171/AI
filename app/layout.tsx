f73ea66b8af4e84eb27509724df53cbb31ad609e
Mahin171 <mahinsoni15@gmail.com>
2023-11-01 11:37:01 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 import { ToasterProvider } from '@/components/toaster-provider'
 import { ModalProvider } from '@/components/modal-provider'
-import { CrispProvider } from '@/components/crisp-provider'
+//import { CrispProvider } from '@/components/crisp-provider'
 
 import './globals.css'
 
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
+  title: 'AI_VERSE',
   description: 'AI Platform',
 }
 
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <CrispProvider />
+        {/* <CrispProvider /> */}
         <body className={font.className}>
           <ToasterProvider />
           <ModalProvider />
--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import TypewriterComponent from "typewriter-effect";
 import Link from "next/link";
 import { useAuth } from "@clerk/nextjs";
 
@@ -12,24 +11,10 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          <TypewriterComponent
-            options={{
-              strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Blog Writing.",
-                "Mail Writing."
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </div>
+        <h1>Your One-Stop AI Platform</h1>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        Create content using AI.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
@@ -38,9 +23,9 @@ export const LandingHero = () => {
           </Button>
         </Link>
       </div>
-      <div className="text-zinc-400 text-xs md:text-sm font-normal">
+      {/* <div className="text-zinc-400 text-xs md:text-sm font-normal">
         No credit card required.
-      </div>
+      </div> */}
     </div>
   );
 };
